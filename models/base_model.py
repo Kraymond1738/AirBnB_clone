6df1abd0d2d19d4aef36b718936a1915facc79b8
Kasumba <kraymond1738@gmail.com>
2023-02-08 09:51:00 +0000

    basemodel2

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
+
 """Basemodel class and the public instance attributes"""
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-	"""This contains  all the common attributes and other classes inherit from this class"""
-	def __init__(self, *args, **kwargs):
-		"""intializing the BaseModel class"""
-		self.id = str(uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+    """
+    This contains  all the common attributes
+    and other classes inherit from this class
+    """
 
-	def __str__(self):
-		"""define a string representation of the object"""
-        	return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        """intializing the BaseModel class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-	def save(self):
-		"""updates the updated_at time"""
-		self.updated_at = datetime.now()
+    def __str__(self):
+        """define a string representation of the object"""
+        name1 = self.__class__.__name__
+        return "[{}] ({}) {}".format(name1, self.id, self.__dict__)
 
-	def to_dict(self):
-		"""returns a dictionary containing all keys/values of __dict__ of the instance"""
-		res = self.__dict__.copy
-		res["__class"] = self.__class__.__name__
-		res["created_at"] = self.created_at.isoformat()
-		res["updated_at"] = self.created_at.isoformat()
-		return res
+    def save(self):
+        """updates the updated_at time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing
+        all keys/values of __dict__ of the instance
+        """
+        res = self.__dict__.copy
+        res["__class"] = self.__class__.__name__
+        res["created_at"] = self.created_at.isoformat()
+        res["updated_at"] = self.created_at.isoformat()
+        return res
