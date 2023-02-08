0cde97ceee2b17529929d3d5df7aaa780bd8558e
Kasumba <kraymond1738@gmail.com>
2023-02-08 09:29:14 +0000

    basemodel1

--- models/base_model.py
+++ models/base_model.py
@@ -7,3 +7,22 @@ class BaseModel:
 	"""This contains  all the common attributes and other classes inherit from this class"""
 	def __init__(self, *args, **kwargs):
 		"""intializing the BaseModel class"""
+		self.id = str(uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+
+	def __str__(self):
+		"""define a string representation of the object"""
+        	return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+	def save(self):
+		"""updates the updated_at time"""
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+		"""returns a dictionary containing all keys/values of __dict__ of the instance"""
+		res = self.__dict__.copy
+		res["__class"] = self.__class__.__name__
+		res["created_at"] = self.created_at.isoformat()
+		res["updated_at"] = self.created_at.isoformat()
+		return res
